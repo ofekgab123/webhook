b418f69e2f714cda22d303daeeb459304f0439ce
KJone1 <davidyahin@protonmail.com>
2023-09-24 13:21:53 +0300

    Add health check route

--- basicWebhook.py
+++ basicWebhook.py
@@ -39,6 +39,10 @@ def webhook():
 
     return jsonify(response), 200
 
+@app.route('/ping', methods=['GET'])
+def webhook():
+    return "pong", 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
